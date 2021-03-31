6b62133888379ebc636fafc2d76f75ac300dabd6
hhm14 <66333988+hhm14@users.noreply.github.com>
2021-03-31 15:48:29 -0500

    Update pa3_bottomup.cpp

--- pa3_bottomup.cpp
+++ pa3_bottomup.cpp
@@ -22,23 +22,18 @@ cout << "init: " << maxdp[0][i] <<", " << mindp[0][i] << endl;
 cout << "init: " << maxdp[i][0] <<", " << mindp[i][0] << endl;
         }
 
-        
-        
-        
         for (int i=1; i < rows; i++) {
             for (int j=1; j < cols; j++) {
 // max of (max right or down, min right or down)
                 maxdp[i][j] = max(max(maxdp[i-1][j] * grid[i][j], mindp[i-1][j] * grid[i][j]),
                                   max(maxdp[i][j-1] * grid[i][j], mindp[i][j-1] * grid[i][j]));
 cout << "max: " << maxdp[i][j] <<", " << mindp[i][j] << endl;
-                
 // min of (max right or down, min right or down)
                 mindp[i][j] = min(min(maxdp[i-1][j] * grid[i][j], mindp[i-1][j] * grid[i][j]),
                                   min(maxdp[i][j-1] * grid[i][j], mindp[i][j-1] * grid[i][j]));
 cout << "min: " << maxdp[i][j] <<", " << mindp[i][j] << endl;
             }
         }
-        
         
 cout << "solution: " << maxdp[rows-1][cols-1] % tennineseven << endl;
 // if solution is negative
